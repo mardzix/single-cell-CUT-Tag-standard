b526c693bc1297f03599998556e0ef7a686d6be7
mardzix <bartosovic.marek@gmail.com>
2021-09-13 11:35:55 +0200

    minor changes in run_pipeline.sh

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -1,6 +1,10 @@
-PIPELINE_FILE="/data/proj/GCB_MB/standard_scCT_preprocess/workflow/snakefile_preprocess"
-CONFIG_FILE="/data/proj/GCB_MB/standard_scCT_preprocess/config/config.yaml"
+PIPELINE_PREFIX=$PWD/$(dirname "$0")/
 
-snakemake --snakefile $PIPELINE_FILE  --cores 32 -p --configfile $CONFIG_FILE $@
+PIPELINE_FILE=$PIPELINE_PREFIX"/../workflow/snakefile_preprocess"
+CONFIG_FILE=$PIPELINE_PREFIX"/../config/config.yaml"
+
+snakemake --snakefile "$PIPELINE_FILE" --cores 32 -p --configfile "$CONFIG_FILE" "$@"
+
+
 
 
