8546cbf7a54f739e0aba58b08cd8c4c3a8b03e4f
marek <bartosovic.marek@gmail.com>
2021-09-14 10:38:08 +0200

    hotfix for add sample to fragments (work with cellranger-atac 2.0)

--- scripts/add_sample_to_fragments.py
+++ scripts/add_sample_to_fragments.py
@@ -9,8 +9,8 @@ sample_name    = sys.argv[2]
 with gzip.open(fragments_file,'rb') as f:
    for line in f:
       line = line.decode('UTF-8').rstrip().split('\t')
+      if line[0].startswith("#"):
+         continue
 
-      if not line[0].startswith("#"):
-         line[3] = sample_name + "_" + line[3]
-
+      line[3] = sample_name + "_" + line[3]
       sys.stdout.write("\t".join(line) + "\n")
